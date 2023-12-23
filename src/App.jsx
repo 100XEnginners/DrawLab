364cffa04dbc16b91e6d74464eaf7c3cadf0421b
Prashant Varma <prashantvarma5083@gmail.com>
2023-12-23 18:18:55 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+// import "./App.css";
+import { Tldraw } from "@tldraw/tldraw";
+
+const App = () =>{
+    return(
+        <div>
+            <Tldraw />
+        </div>
+    )
+}
+
+export default App;
